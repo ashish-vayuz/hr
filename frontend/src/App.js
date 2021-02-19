9749ea8561739a0ff4b812b1cdada8256c98a41c
smackzack-vayuz <ashish.raj@vayuz.com>
2021-02-19 14:25:28 +0530

    added the json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
 
 class App extends Component {
 
@@ -23,8 +22,6 @@ class App extends Component {
           <React.Suspense fallback={loading}>
             <Switch>
               <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
-              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
               <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
             </Switch>
           </React.Suspense>
