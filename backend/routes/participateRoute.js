5ba679b4e354aa88b4b6eb1c56466c3408c2332c
smackzack-vayuz <ashish.raj@vayuz.com>
2021-03-27 15:07:23 +0530

    'ds

--- backend/routes/participateRoute.js
+++ backend/routes/participateRoute.js
@@ -39,6 +39,7 @@ const upload = multer({
 router.route('/upload').post(upload.single('video'), uploadChal)
 router.route('/like/:id').get(protect, likeChallengeById)
 router.route('/unlike/:id').get(protect, unlikeChallengeById)
+router.route('/admin/:id').put(protect, sendAdminParticipation)
 router.route('/')
     .get(protect, getPaticipation)
 router.route('/:id').put(protect, updateParticipation).get(protect, getParticipationById)
