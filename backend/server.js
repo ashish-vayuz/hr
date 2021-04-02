4e3e7484f13c8058ff53d936e6c7a8937a3ad3ae
Atul Kumar Gautam <atul.gautam@vayuz.com>
2021-04-02 09:18:34 +0530

    inital

--- backend/server.js
+++ backend/server.js
@@ -1,60 +1,61 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { notFound, errorHandler } from './middlewares/errorMiddlewares.js'
-import connectDB from './config/db.js'
-import cors from 'cors'
-import path from 'path'
-import morgan from 'morgan'
+import express from "express";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middlewares/errorMiddlewares.js";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import path from "path";
+import morgan from "morgan";
 
 //Routes
-import userRoutes from './routes/userRoutes.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import challengeRoutes from './routes/challengeRoute.js'
-import participate from './routes/participateRoute.js'
-import adminRoutes from './routes/adminRoutes.js'
-import cmsRoutes from './routes/cmsRoutes.js'
-import googleRoutes from './config/google.js'
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import challengeRoutes from "./routes/challengeRoute.js";
+import participate from "./routes/participateRoute.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import cmsRoutes from "./routes/cmsRoutes.js";
+import googleRoutes from "./config/google.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
-const app = express()
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+connectDB();
+const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-
-app.use('/users', userRoutes)
-app.use('/category', categoryRoutes)
-app.use('/challenge', challengeRoutes)
-app.use('/participation', participate)
-app.use('/admin', adminRoutes)
-app.use('/google', googleRoutes)
+app.use("/users", userRoutes);
+app.use("/category", categoryRoutes);
+app.use("/challenge", challengeRoutes);
+app.use("/participation", participate);
+app.use("/admin", adminRoutes);
+app.use("/google", googleRoutes);
 app.use("/cms", cmsRoutes);
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    )
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
-    app.get('/', (req, res) => {
-        res.send('API is running....')
-    })
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
 }
 
-
-
 //Error Middlewares
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
